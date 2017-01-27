789bbdea2a04c4a7027ff36a9cbe272636d62ad4
northmyrtle <todd@northmyrtle.net>
2017-01-27 02:23:26 -0600

    Merge origin/master
    Conflicts:
    	www/To_Do_20161106.txt

--- public_html/js/pbr_demo.js
+++ public_html/js/pbr_demo.js
@@ -1,70 +1,14 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+ 
+console.log('loaded pbr_demo.js');
+document.addEventListener("deviceready", onDeviceReady, false);
+ 
+ 
+ function onDeviceReady() {
 
+            console.log('device ready');
+            document.getElementById("main").innerHTML='<object type="text/html" data="http://www.google.com" ></object>';
 
-var inAppBrowserRef;
-
-function showHelp(url) {
-
-    var target = "_blank";
-
-    var options = "location=yes,hidden=yes";
-
-    inAppBrowserRef = cordova.InAppBrowser.open(url, target, options);
-
-    inAppBrowserRef.addEventListener('loadstart', loadStartCallBack);
-
-    inAppBrowserRef.addEventListener('loadstop', loadStopCallBack);
-
-    inAppBrowserRef.addEventListener('loaderror', loadErrorCallBack);
-
-}
-
-function loadStartCallBack() {
-
-    $('#status-message').text("loading please wait ...");
-
-}
-
-function loadStopCallBack() {
-
-    if (inAppBrowserRef != undefined) {
-
-        inAppBrowserRef.insertCSS({ code: "body{font-size: 25px;" });
-
-        $('#status-message').text("");
-
-        inAppBrowserRef.show();
-    }
-
-}
-
-function loadErrorCallBack(params) {
-
-    $('#status-message').text("");
-
-    var scriptErrorMesssage =
-       "alert('Sorry we cannot open that page. Message from the server is : "
-       + params.message + "');"
-
-    inAppBrowserRef.executeScript({ code: scriptErrorMesssage }, executeScriptCallBack);
-
-    inAppBrowserRef.close();
-
-    inAppBrowserRef = undefined;
-
-}
-
-function executeScriptCallBack(params) {
-
-    if (params[0] == null) {
-
-        $('#status-message').text(
-           "Sorry we couldn't open that page. Message from the server is : '"
-           + params.message + "'");
-    }
-
-}
+            };
+            
+            
+ document.addEventListener("deviceready", onDeviceReady, false);
